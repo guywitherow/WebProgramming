01cab2e894057672b45617316881e2f517ccfe23
s3783428 <s3783428@student.rmit.edu.au>
2019-08-26 10:46:02 +1000

    Set up basic page layout

--- a2/index.php
+++ a2/index.php
@@ -4,34 +4,39 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Assignment 2</title>
-    
+
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Righteous&display=swap" rel="stylesheet">
     <script src='../wireframe.js'></script>
   </head>
 
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div id="tital"><img id="logo" src="../../media/moviehouse.png" alt="Logo of Lunardo Cinema">Lunardo</div>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <ul class="navList">
+        <li class="navItem">a</li>
+        <li class="navItem">b</li>
+        <li class="navItem">c</li>
+      </ul>
     </nav>
 
     <main>
       <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+          <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+          <!-- <img src='../../media/website-under-construction.png' alt='Website Under Construction' /> -->
+        </article>
     </main>
 
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Guy Witherow s3783428, Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
