894b807be41ad5d903a624671259ec4536bf04d1
s3783428 <s3783428@student.rmit.edu.au>
2019-08-29 14:14:58 +1000

    layout and synopsis added

--- a2/index.php
+++ a2/index.php
@@ -10,23 +10,21 @@
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link href="https://fonts.googleapis.com/css?family=Lato|Righteous&display=swap" rel="stylesheet">
     <script src='../wireframe.js'></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
   <body>
 
-    <header>
-      <div>
-        <img id="logo" src="../../media/lunardo_logo_no_text.png" alt="Logo of Lunardo Cinema">
-        <span id="title">Lunardo</span>
-      </div>
+    <header class="wide100">
+      <img id="logo" src="../../media/lunardo_logo_no_text.png" alt="Logo of Lunardo Cinema">
+      <span id="title">Lunardo</span>
     </header>
 
     <nav>
-      <ulss="navList">
-        <li class="navItem"><a href="#aboutUs">About Us</a></li>
-        <li class="navItem"><a href="#prices">Prices</a></li>
-        <li class="navItem"><a href="#nowShowing">Now Showing</a></li>
-
+      <ul class="navList">
+        <a href="#aboutUs"><li class="navItem">About Us</li></a>
+        <a href="#prices"><li class="navItem">Prices</li></a>
+        <a href="#nowShowing"><li class="navItem last">Now Showing</li></a>
       </ul>
     </nav>
 
@@ -46,7 +44,7 @@
             <div class="imageHalf">
               <img src="../../media/firstClassSeat.png" class="seatImage imageCentre">
               <h3>First Class Seating</h3>
-              <p>The new standard seating sports luxurious cushioning, as well as adjustable armrests with cupholders!
+              <p>First Class takes comfort to a new level, with leg rests that raise, comforting you through your first class experience.
             </div>
           </div>
         </div>
@@ -55,126 +53,188 @@
 
         <div class="sub_article">
           <h3>Revamped Sound and Visuals</h3>
-
           <div class="inlineContainer">
             <div class="inline inlineLeft">
               <img id="dolbyAtmos" class="imageCentre" src="../../media/atmos.png">
             </div>
             <div class="inline inlineRight">
-              <ul class="dolbyFactList">
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-              </ul>
-              <ul class="dolbyFactList">
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-              </ul>
+              <div class="factContainer">
+                <ul class="dolbyFactList">
+                  <li class="dolbyFact">
+                    <h4>All sides covered</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                  <li class="dolbyFact">
+                    <h4>All sides covered</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div class="factContainer">
+                <ul class="dolbyFactList">
+                  <li class="dolbyFact">
+                    <h4>All sides covered</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                  <li class="dolbyFact">
+                    <h4>All sides covered</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-
           <div class="inlineContainer dolbyBorder">
             <div class="inline inlineLeft">
               <img id="dolbyAtmos" class="imageCentre" src="../../media/vision.png">
             </div>
             <div class="inline inlineRight">
-              <ul class="dolbyFactList">
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-              </ul>
-              <ul class="dolbyFactList">
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-                <li class="dolbyFact">
-                  <h4>All sides covered</h4>
-                  <p>
-                    The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
-                  </p>
-                </li>
-              </ul>
+              <div class="factContainer">
+                <ul class="dolbyFactList">
+                  <li class="dolbyFact">
+                    <h4>All sides covered1</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                  <li class="dolbyFact">
+                    <h4>All sides covered2</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div class="factContainer">
+                <ul class="dolbyFactList">
+                  <li class="dolbyFact">
+                    <h4>All sides covered3</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                  <li class="dolbyFact">
+                    <h4>All sides covered4</h4>
+                    <p>
+                      The movies comes alive as sound is produced on all sides, including above you, whisking the whole cinema into the world of the movie.
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </div>
+          <div class="dolbyFooter">
+            <p>For more information, visit the <a href="https://www.dolby.com/us/en/cinema/index.html" target="_blank">Dolby Site</a>.</p>
+          </div>
         </div>
-        <p>For more information, visit the <a href="https://www.dolby.com/us/en/cinema/index.html" target="_blank">Dolby Site</a>.</p>
       </article>
-
       <article class="article" id="prices">
-
         <div class="gridContainer">
           <div class="title">
             <h1>Prices</h1>
           </div>
           <div class="subtitle">
-            Special prices apply to midday sessions, Monday to Friday, as well as Monday and Wednesday.
+            Special prices apply to midday sessions on Monday to Friday, as well as all day Monday and Wednesday.
           </div>
           <div class="std1 standard">
-            Standard Adult:
-            <p> Standard: $19.80
-            <p> Special: $14.00
+            <h3>Standard Adult</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $19.80</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $14.00</span>
+            </div>
           </div>
           <div class="std2 standard">
-            Standard Concession:
-            <p> Standard: $17.50
-            <p> Special: $12.50
+            <h3>Standard Concession</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $17.50</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $12.50</span>
+            </div>
           </div>
           <div class="std3 standard">
-            Standard Child:
-            <p> Standard: $15.30
-            <p> Special: $11.00
+            <h3>Standard Child</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $15.30</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $11.00</span>
+            </div>
           </div>
           <div class="first1 first">
-            First Class Adult:
-            <p> Standard: $30.00
-            <p> Special: $24.00
+            <h3>First Class Adult</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $30.00</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $24.00</span>
+            </div>
           </div>
           <div class="first2 first">
-            First Class Concession:
-            <p> Standard: $27.00
-            <p> Special: $22.50
+            <h3>First Class Concession</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $27.00</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $22.50</span>
+            </div>
           </div>
           <div class="first3 first">
-            First Class Child:
-            <p> Standard: $24.00
-            <p> Special: $21.00
+            <h3>First Class Child</h3>
+            <div class="standardPrice imageCentre">
+              <span>Standard: $24.00</span>
+            </div>
+            <div class="specialPrice imageCentre">
+              <span>Special: $21.00</span>
+            </div>
           </div>
         </div>
       </article>
 
       <article class="article" id="nowShowing">
-        <h1>Now Showing</h1>
-
+        <div class="articleContainer">
+          <h1>Now Showing</h1>
+          <div class="posterContainer">
+            <div class="avengers postCard"><img class="poster" src="../../media/avengers.png"></div>
+            <div class="tEW postCard"><img class="poster" src="../../media/topEndWedding.png"></div>
+            <div class="dumbo postCard"><img class="poster" src="../../media/dumbo.png"></div>
+            <div class="tHP postCard"><img class="poster" src="../../media/theHappyPrince.png"></div>
+          </div>
+          <div class="synopsisContainer">
+            <div top>
+              <div left 1/2>
+                <div top>
+                  Avengers: Endgame     PG
+                </div>
+                <div bottom>
+                  After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.
+                </div>
+              </div>
+              <div right 1/2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/TcMBFSGVi1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+            <div bottom>
+              <div left 1/3>
+                Make A Booking
+              </div>
+              <div right 2/3>
+                <button>Wednesday 9pm</button><button>Thursday 9pm</button><button>Friday 9pm</button>
+                <button>Saturday 6pm</button><button>Sunday 6pm</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </article>
     </main>
 
